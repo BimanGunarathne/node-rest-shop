acde77ceb16507d83ebad51357afdb7e2ef68c9c
Biman Gunarathne <bimangunarathne@outlook.com>
2023-12-29 18:59:06 +0530

    install mongoose and connect the database

--- api/routes/products.js
+++ api/routes/products.js
@@ -11,7 +11,6 @@ router.post('/', (req, res, next) => {
     const product = {
         name: req.body.name,
         price: req.body.price,
-
     };
     res.status(201).json({
         message: 'Handling POST requests to /products',
--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop.pdekhfz.mongodb.net/?retryWrites=true&w=majority', {
+    useMongoClient: true
+})
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
