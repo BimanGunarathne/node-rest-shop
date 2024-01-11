8322830a789d4ee473b8ae7bb0f3784449b73c55
Biman Gunarathne <bimangunarathne@outlook.com>
2024-01-11 08:56:45 +0530

    add authentication

--- api/routes/products.js
+++ api/routes/products.js
@@ -20,7 +20,7 @@ const fileFilter = (req, file, cb) => {
     if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
         cb(null, true);
     } else {
-        cb(nll, false);
+        cb(null, false);
     }
 }
 
@@ -67,7 +67,7 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', upload.single('productImage'), checkAuth, (req, res, next) => {
+router.post('/', checkAuth, upload.single('productImage'), (req, res, next) => {
     console.log(req.file);
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
@@ -121,7 +121,7 @@ router.get('/:productId', (req, res, next) => {
         });
 });
 
-router.patch('/:productId', (req, res, next) => {
+router.patch('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     const updateOps = {};
     for (const ops of req.body) {
@@ -141,7 +141,7 @@ router.patch('/:productId', (req, res, next) => {
         })
     })
 });
-router.delete('/:productId', (req, res, next) => {
+router.delete('/:productId', checkAuth, (req, res, next) => {
     const id = req.params.productId;
     Product.deleteOne({ _id: id })
         .exec()
